cb584f8edd99aee4abd0b17079fcab71b927dfac
ayaancode999 <250837490+ayaancode999@users.noreply.github.com>
2026-05-16 20:49:49 +0100

    configured dot env file

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlmodel import SQLModel,create_engine,Session 
+import os
 
-DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/car_service_dev_db'
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine=create_engine(DATABASE_URL, echo=True)
 
